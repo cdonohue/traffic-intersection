68b34acbb29c7b0dbdc9a6e4cf5995be4a357b23
cdonohue <chad.donohue@gmail.com>
2024-01-12 12:24:23 -0600

    Integrate machine into component

--- components/Intersection.tsx
+++ components/Intersection.tsx
@@ -1,11 +1,51 @@
+'use client'
+
+import { useMachine } from '@xstate/react'
 import TrafficLight from './TrafficLight'
+import intersectionMachine from '@/machines/intersectionMachine'
 
 export default function Intersection() {
+  const [currentState, send] = useMachine(intersectionMachine)
+
+  function getLightState(direction: string) {
+    const { value } = currentState
+
+    let straightState = 'stop'
+
+    if (value === 'northSouthGreen') {
+      if (direction === 'north' || direction === 'south') {
+        straightState = 'go'
+      }
+    } else if (value === 'eastWestGreen') {
+      if (direction === 'east' || direction === 'west') {
+        straightState = 'go'
+      }
+    } else if (value === 'northSouthYellow') {
+      if (direction === 'north' || direction === 'south') {
+        straightState = 'caution'
+      }
+    } else if (value === 'eastWestYellow') {
+      if (direction === 'east' || direction === 'west') {
+        straightState = 'caution'
+      }
+    } else if (value === 'northSouthRed') {
+      if (direction === 'north' || direction === 'south') {
+        straightState = 'stop'
+      }
+    } else if (value === 'eastWestRed') {
+      if (direction === 'east' || direction === 'west') {
+        straightState = 'stop'
+      }
+    }
+
+    return { straightState }
+  }
+
   return (
     <div className="grid grid-cols-4 grid-rows-4">
       <div className="bg-green-700 rounded-br-2xl"></div>
       <div className="rotate-180">
-        <TrafficLight data-direction="south" />
+        <TrafficLight {...getLightState('south')} />
       </div>
       <div className="border-l-4 border-zinc-700"></div>
       <div className="bg-green-700 rounded-bl-2xl"></div>
@@ -13,10 +53,10 @@ export default function Intersection() {
       <div />
       <div />
       <div className="-rotate-90">
-        <TrafficLight data-direction="west" />
+        <TrafficLight {...getLightState('west')} />
       </div>
       <div className="rotate-90">
-        <TrafficLight data-direction="east" />
+        <TrafficLight {...getLightState('east')} />
       </div>
       <div />
       <div />
@@ -24,7 +64,7 @@ export default function Intersection() {
       <div className="bg-green-700 rounded-tr-2xl"></div>
       <div className="border-r-4 border-zinc-700"></div>
       <div className="rotate-0">
-        <TrafficLight data-direction="north" />
+        <TrafficLight {...getLightState('north')} />
       </div>
       <div className="bg-green-700 rounded-tl-2xl"></div>
     </div>
