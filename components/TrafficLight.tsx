f987c0c37e63a2d6d39812c9333b41c85280d2ed
cdonohue <chad.donohue@gmail.com>
2024-01-12 09:06:16 -0600

    Add TrafficLight component

---
+++ components/TrafficLight.tsx
@@ -0,0 +1,69 @@
+import { ArrowLeftIcon } from '@heroicons/react/16/solid'
+
+type Props = {
+  straightState: 'stop' | 'caution' | 'go'
+  turnState: 'stop' | 'caution' | 'go' | 'yield'
+}
+
+export default function TrafficLight({
+  straightState = 'stop',
+  turnState = 'stop',
+}: Props) {
+  return (
+    <div className="bg-zinc-900 p-3 rounded-lg flex gap-2">
+      <ArrowLight state={turnState} />
+      <SolidLight color="red" isOn={straightState === 'stop'} />
+      <SolidLight color="yellow" isOn={straightState === 'caution'} />
+      <SolidLight color="green" isOn={straightState === 'go'} />
+    </div>
+  )
+}
+
+type SolidLightProps = {
+  color: 'red' | 'yellow' | 'green'
+  isOn?: boolean
+}
+
+function SolidLight({ color, isOn = false }: SolidLightProps) {
+  if (!isOn) {
+    return <div className="w-8 h-8 rounded-full bg-zinc-800" />
+  }
+
+  const bg =
+    color === 'red'
+      ? 'bg-red-500'
+      : color === 'yellow'
+      ? 'bg-yellow-500'
+      : color === 'green'
+      ? 'bg-green-500'
+      : 'bg-zinc-600'
+
+  return <div className={`w-8 h-8 rounded-full transition-colors ${bg}`} />
+}
+
+type ArrowLightProps = {
+  state: 'stop' | 'caution' | 'go' | 'yield'
+}
+
+function ArrowLight({ state }: ArrowLightProps) {
+  const color =
+    state === 'go'
+      ? 'text-green-500'
+      : state === 'caution'
+      ? 'text-yellow-500'
+      : state === 'stop'
+      ? 'text-red-500'
+      : state === 'yield'
+      ? 'text-orange-500'
+      : 'text-zinc'
+
+  return (
+    <div className="w-8 h-8 rounded-full flex items-center justify-center bg-zinc-800">
+      <ArrowLeftIcon
+        className={`w-6 h-6 ${color} ${
+          state === 'yield' ? 'animation-blink' : ''
+        }`}
+      />
+    </div>
+  )
+}
