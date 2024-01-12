5d221b80917233cb7a04806dcec769c757957b7c
cdonohue <chad.donohue@gmail.com>
2024-01-12 13:23:29 -0600

    Add turn states

--- machines/intersectionMachine.ts
+++ machines/intersectionMachine.ts
@@ -6,37 +6,77 @@ export default createMachine({
   states: {
     allRed: {
       after: {
+        2000: 'northGreenArrow',
+      },
+    },
+    northGreenArrow: {
+      after: {
+        5000: 'northYellowArrow',
+      },
+    },
+    northYellowArrow: {
+      after: {
         2000: 'northSouthGreen',
       },
     },
     northSouthGreen: {
       after: {
-        5000: 'northSouthYellow',
+        5000: 'northYellow',
       },
     },
-    northSouthYellow: {
+    northYellow: {
+      after: {
+        2000: 'southGreenArrow',
+      },
+    },
+    southGreenArrow: {
+      after: {
+        5000: 'southYellowArrow',
+      },
+    },
+    southYellowArrow: {
       after: {
         2000: 'allRedBeforeEastWest',
       },
     },
     allRedBeforeEastWest: {
       after: {
-        1000: 'eastWestGreen',
+        1000: 'eastGreenArrow',
+      },
+    },
+    eastGreenArrow: {
+      after: {
+        5000: 'eastYellowArrow',
+      },
+    },
+    eastYellowArrow: {
+      after: {
+        2000: 'eastWestGreen',
       },
     },
     eastWestGreen: {
       after: {
-        5000: 'eastWestYellow',
+        5000: 'eastYellow',
       },
     },
-    eastWestYellow: {
+    eastYellow: {
+      after: {
+        2000: 'westGreenArrow',
+      },
+    },
+    westGreenArrow: {
+      after: {
+        5000: 'westYellowArrow',
+      },
+    },
+    westYellowArrow: {
       after: {
         2000: 'allRedBeforeNorthSouth',
       },
     },
     allRedBeforeNorthSouth: {
       after: {
-        1000: 'northSouthGreen',
+        1000: 'northGreenArrow',
       },
     },
   },
